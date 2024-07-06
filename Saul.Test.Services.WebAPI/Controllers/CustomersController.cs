013b0ac17317bd33b93a66516666990883d9449d
Saul Chipana <Saul.Dev09@gmail.com>
2024-07-06 16:51:56 +0200

    Service Layer

---
+++ Saul.Test.Services.WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,79 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Saul.Test.Application.DTO;
+using Saul.Test.Application.Interface;
+
+namespace Saul.Test.Services.WebAPI.Controllers
+{
+    [Route("api/{controller}/{action}")]
+    [ApiController]
+    public class CustomersController : Controller
+    {
+        private readonly ICustomersApplication _customersApplication;
+
+        public CustomersController(ICustomersApplication customersApplication)
+        {
+            _customersApplication = customersApplication;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _customersApplication.GetAll();
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> Get(string customerId)
+        {
+            var response = await _customersApplication.Get(customerId);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert([FromBody] CustomersDto customersDto)
+        {
+            if (customersDto == null)
+                return BadRequest();
+
+            var response = await _customersApplication.Insert(customersDto);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] CustomersDto customersDto)
+        {
+            if (customersDto == null)
+                return BadRequest();
+
+            var response = await _customersApplication.Update(customersDto);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpDelete("{customerId}")]
+        public async Task<IActionResult> Delete(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+                return BadRequest();
+
+            var response = await _customersApplication.Delete(customerId);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+    }
+}
--- Saul.Test.Services.WebAPI/Startup.cs
+++ Saul.Test.Services.WebAPI/Startup.cs
@@ -9,6 +9,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Saul.Test.Transversal.Mapper;
+using Saul.Test.Transversal.Common;
+using Saul.Test.Infrastructure.Data;
+using Saul.Test.Application.Interface;
+using Saul.Test.Application.Main;
+using Saul.Test.Domain.Interface;
+using Saul.Test.Domain.Core;
+using Saul.Test.Infrastructure.Interface;
+using Saul.Test.Infrastructure.Repository;
 
 namespace Saul.Test.Services.WebAPI
 {
@@ -24,7 +34,15 @@ namespace Saul.Test.Services.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddAutoMapper(x => x.AddProfile(new MappingsProfile()));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options => { options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver(); });
+
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddSingleton<IConnectionFactory, ConnectionFactory>();
+            services.AddScoped<ICustomersApplication, CustomersApplication>();
+            services.AddScoped<ICustomersDomain, CustomersDomain>();
+            services.AddScoped<ICustomersRepository, CustomersRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
