0d40fa2f8a9e133f8a05bd0edcf3d9c06662b32c
Saul Chipana <Saul.Dev09@gmail.com>
2024-07-10 04:42:51 +0200

    Get Users - Added

---
+++ Saul.Test.Domain.Core/UsersDomain.cs
@@ -0,0 +1,24 @@
+﻿using Saul.Test.Domain.Entity;
+using Saul.Test.Domain.Interface;
+using Saul.Test.Infrastructure.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Saul.Test.Domain.Core
+{
+    public class UsersDomain : IUsersDomain
+    {
+        private readonly IUsersRepository _usersRepository;
+
+        public UsersDomain(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        public Users Authenticate(string userName, string password)
+        {
+            return _usersRepository.Authenticate(userName, password);
+        }
+    }
+}
