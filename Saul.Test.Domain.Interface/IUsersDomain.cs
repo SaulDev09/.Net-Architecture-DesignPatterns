74fa2c08180d9978e0032f3b749171c5e7332e4d
Saul Chipana <Saul.Dev09@gmail.com>
2024-07-31 22:26:42 +0200

    40 Clean Architecture 03, Cleaning Folders

--- Saul.Test.Domain.Interface/IUsersDomain.cs
+++
@@ -1,9 +0,0 @@
-﻿using Saul.Test.Domain.Entity;
-
-namespace Saul.Test.Domain.Interface
-{
-    public interface IUsersDomain
-    {
-        Users Authenticate(string userName, string password);
-    }
-}
